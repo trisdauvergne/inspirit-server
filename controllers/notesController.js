ccf78ff415f741e80059d964cd477b412e2c4f75
Tristan Dauvergne <tristan.dauvergne@gmail.com>
2021-05-21 13:55:50 +0200

    Added video functionality for posting YouTube embed links

--- controllers/notesController.js
+++ controllers/notesController.js
@@ -8,12 +8,28 @@ const messageData = [
 export const newNote = async (req, res) => {
   console.log('in notes controller');
   let input = req.body.message;
-  let newMessage = {
-    message: input,
-    id: Math.random() * 1000,
-  };
-  console.log('the new message is, ', newMessage);
-  messageData.push(newMessage);
+  const youtube = /youtube/i;
+  if (youtube.test(input)) {
+    let newMessage = {
+      video: input,
+      id: Math.random() * 1000
+    };
+    console.log('the video is', newMessage);
+    messageData.push(newMessage);
+  } else {
+    let newMessage = {
+      message: input,
+      id: Math.random() * 1000,
+    };
+    console.log('the new message is, ', newMessage);
+    messageData.push(newMessage);
+  }
+  // let newMessage = {
+  //   message: input,
+  //   id: Math.random() * 1000,
+  // };
+  // console.log('the new message is, ', newMessage);
+  // messageData.push(newMessage);
   res.end();
 };
 
